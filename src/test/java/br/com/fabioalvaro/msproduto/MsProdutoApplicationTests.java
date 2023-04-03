d4c74c1413f6a87f4706fcbf12497d5d8b5923ce
Fabiao <fabio.alvaro@portoseguro.com.br>
2023-04-03 19:27:06 -0300

    subindo o inicio

---
+++ src/test/java/br/com/fabioalvaro/msproduto/MsProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabioalvaro.msproduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
